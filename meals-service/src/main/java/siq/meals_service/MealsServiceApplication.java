e43d9f6c25e895b7451427b5157456d231f39a9e
Caio Bukvar <bukvar2009@gmail.com>
2025-04-04 10:08:37 -0300

    project start

---
+++ meals-service/src/main/java/siq/meals_service/MealsServiceApplication.java
@@ -0,0 +1,13 @@
+package siq.meals_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealsServiceApplication.class, args);
+	}
+
+}
---
+++ meals-service/src/test/java/siq/meals_service/MealsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package siq.meals_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
